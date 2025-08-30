3b98f3063c006a0472e477accff187487ea1c18e
João Victor Fernandes Mendes <joaovictorf01@gmail.com>
2025-08-30 06:30:29 -0300

    iniciando projeto.

---
+++ msdicamentos/src/main/java/fiap/msmedicamentos/MsMedicamentosApplication.java
@@ -0,0 +1,11 @@
+package fiap.msmedicamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsMedicamentosApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MsMedicamentosApplication.class, args);
+    }
+}
