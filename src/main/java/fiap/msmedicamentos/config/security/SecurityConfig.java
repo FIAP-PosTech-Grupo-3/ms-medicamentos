d85ae9cc4c2e5b09bcf4a450fc5871fbd2949f3f
João Victor Fernandes Mendes <joaovictorf01@gmail.com>
2025-09-12 20:58:41 -0300

    ajuste  estrutura de pasta.

--- src/main/java/fiap/msmedicamentos/config/security/SecurityConfig.java
+++ src/main/java/fiap/msmedicamentos/config/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package fiap.msmedicamentos.config.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -41,7 +42,7 @@ public class SecurityConfig {
                 .requestMatchers(HttpMethod.DELETE, "/api/**").hasRole("ADMIN")
                 .anyRequest().authenticated()
             )
-            .httpBasic(httpBasic -> {})
+            .httpBasic(Customizer.withDefaults())
             .userDetailsService(userDetailsService);
 
         return http.build();
